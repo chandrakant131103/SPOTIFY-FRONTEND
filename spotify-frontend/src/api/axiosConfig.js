49fe68756e1329af6b38b2362dbb8e48cd250585
CHANDRAKANT BARIK <fr16chandrakant@gmail.com>
2026-03-11 18:31:59 +0530

    Update axiosConfig.js

--- spotify-frontend/src/api/axiosConfig.js
+++ spotify-frontend/src/api/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: ' https://spotify-backend-77s2.onrender.com', // Your Express backend URL
+    baseURL: 'https://spotify-backend-77s2.onrender.com/api', // Your Express backend URL
     withCredentials: true // Sends the JWT cookie with every request
 });
 
