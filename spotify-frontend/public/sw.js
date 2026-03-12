622df222dc0b66dcb6254987e3c932b18c0a1d3f
CHANDRAKANT BARIK <fr16chandrakant@gmail.com>
2026-03-13 01:29:13 +0530

    Update sw.js

--- spotify-frontend/public/sw.js
+++ spotify-frontend/public/sw.js
@@ -1,1 +1,8 @@
-self.addEventListener('fetch', () => {});
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Desktop Chrome needs this fetch handler to enable the install icon
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
