cda11939875fff62313cf71ee4c9b88b5b164efb
CHANDRAKANT BARIK <fr16chandrakant@gmail.com>
2026-03-13 01:37:05 +0530

    Update sw.js

--- spotify-frontend/public/sw.js
+++ spotify-frontend/public/sw.js
@@ -1,12 +1,1 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
-});
-
-// Mandatory for PWA Install button to show up
-self.addEventListener('fetch', (event) => {
-  // Can be empty, but MUST exist
-});
+self.addEventListener('fetch', () => {});
